54219dfed1b7ba65f6f5980b29acde96f57fd5a8
irushiuk <irushiudyani@gmail.com>
2024-08-15 18:53:56 +0530

    footer updated

--- frontend/client/src/component/Footer.jsx
+++ frontend/client/src/component/Footer.jsx
@@ -2,10 +2,10 @@
 const Footer = () => {
   return (
     <>
-    <div className="nt-8 w-full bg-black px-8 md:px-[500px] flx justify-between
+    <div className="nt-8 w-full bg-slate-500 px-8 md:px-[500px] flx justify-between
     md:text-md py-8">
-        <div className="flex-col text-white">
-            <p>Most viwed</p>
+        <div className="flex-col text-center text-white ">
+            <p>Traveler's Blog</p>
 
         </div>
     </div>
