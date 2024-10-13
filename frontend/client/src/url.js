b76718df6f8c74637c9641f6b24af512af320965
irushiuk <irushiudyani@gmail.com>
2024-10-13 12:54:43 +0530

    backend link added

--- frontend/client/src/url.js
+++ frontend/client/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:5000"
-export const If = "http://localhost:5000/images/"
+export const URL =" https://blog-app-7chw.onrender.com"
+export const If = "https://blog-app-7chw.onrender.com/images/"
