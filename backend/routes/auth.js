53ab1129bc2b8d7a0c9876c20464ff32dba5bbc2
irushiuk <irushiudyani@gmail.com>
2024-10-13 12:34:58 +0530

    bcryptjs installed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); // Ensure the correct path
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // REGISTER
