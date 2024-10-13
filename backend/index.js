682070029bf9ca6835dd383eb9b8dafd56278309
irushiuk <irushiudyani@gmail.com>
2024-10-13 14:17:34 +0530

    link updated

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"https://blog-app-7chw.onrender.com",credentials:true}))
+app.use(cors({origin:"https://blog-app-frontend-sp97.onrender.com",credentials:true}))
 
 // Use the user routes
 app.use('/api/auth', authRoute);
